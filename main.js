0d4019d8012ee4231a329d140fab139321e3a8f0
tomaszabala <tomas_zab@outlook.com>
2022-06-17 21:20:28 -0300

    Galería
    Empezando con la galería

--- main.js
+++ main.js
@@ -9,11 +9,3 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');          //con la propiedad "toggle" si el elemento tiene una clase se la va a quitar, y si no tiene se la va a agregar
     btnSwitch.classList.toggle('active');
 })
-
-// // menu servicios
-// const btnServicios = document.querySelector('servicios')
-
-// btnServicios.addEventListener('click', () => {
-//     document.body.classList.add('menu');
-//     btnServicios.classList.add('active');
-// })
