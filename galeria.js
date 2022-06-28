1ac33915e52d5a1cfdc38d52d9fb5c1a729a3d01
tomaszabala <tomas_zab@outlook.com>
2022-06-28 20:49:42 -0300

    galeria

--- galeria.js
+++ galeria.js
@@ -1,6 +1,11 @@
 const modalimgBox = document.getElementById("modal-imgBox");
 const modalimg = document.getElementById("modal-img");
 
+function openModalImg(reference) {
+    modalimgBox.style.display = "flex";
+    modalimg.src = reference;
+}
+
 function closeImg() {
     modalimgBox.style.display = "none";
 }
