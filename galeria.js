3b5efa9b91ad0379e104042fa5c2eeaf319a5da3
tomaszabala <tomas_zab@outlook.com>
2022-06-28 19:42:52 -0300

    Galeria
    sigo con galeria

---
+++ galeria.js
@@ -0,0 +1,6 @@
+const modalimgBox = document.getElementById("modal-imgBox");
+const modalimg = document.getElementById("modal-img");
+
+function closeImg() {
+    modalimgBox.style.display = "none";
+}
