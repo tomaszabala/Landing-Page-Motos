f972486360050daf0d3ee08713101c16346d8fd2
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2022-10-14 14:51:11 -0300

    navBar fix

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
+function openDrawerMenu() {
+  var x = document.getElementById("mainNavBar");
+  if (x.className === "navBar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navBar";
+  }
+}
 
+// // Dark mode
+// const btnSwitch = document.querySelector("#switch");
 
-
-
-// Dark mode
-const btnSwitch = document.querySelector('#switch')
-
-btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');          //con la propiedad "toggle" si el elemento tiene una clase se la va a quitar, y si no tiene se la va a agregar
-    btnSwitch.classList.toggle('active');
-})
+// btnSwitch.addEventListener("click", () => {
+//   document.body.classList.toggle("dark"); //con la propiedad "toggle" si el elemento tiene una clase se la va a quitar, y si no tiene se la va a agregar
+//   btnSwitch.classList.toggle("active");
+// });
